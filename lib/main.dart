4a01df437afcb01ea09e53fd00e947ebb339973e
Widyarso Joko Purnomo <widyjp10@gmail.com>
2022-04-03 19:26:32 +0700

    change web config

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'WOTLA',
       theme: ThemeData(
         colorScheme: const ColorScheme(
           brightness: Brightness.light,
